d4ea5cb0e5f8323288412c8dc42f2540140b3f1e
Rocketz64 <alfaiqrido@gmail.com>
2025-12-10 22:09:49 +0700

    Kode Pertemuan 5

---
+++ Pertemuan5/thread1.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define _REENTRANT
+
+
+void *ThreadRoutine(int number)
+{
+while(1)
+{
+printf("pid %d : thread %d running\n",getpid(), number);
+sleep(number);
+}
+}
+int main(void)
+{
+int t;
+pthread_t tid[5];
+for (t=1; t<5; t++)
+pthread_create(&tid[t],NULL,(void *)ThreadRoutine, &t);
+while(1)
+{
+printf("pid %d : parent running\n", getpid());
+sleep(10);
+}
+exit(1);
+}
